54101f7c8da6387a081985f9a3e39490aaf1ab73
hagome <gktkd950816@naver.com>
2019-04-06 02:22:59 +0900

    boot2 oauth2 login 구현

---
+++ src/Spring-Boot2-Community-Web/src/main/java/com/web/controller/LoginController.java
@@ -0,0 +1,36 @@
+package com.web.controller;
+
+import com.web.annotation.SocialUser;
+import com.web.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+//    @GetMapping(value = "/facebook|google|kakao/complete")
+//    public String loginComplete(HttpSession session) {
+//        OAuth2Authentication authentication = (OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication();
+//        Map<String, String> map = (HashMap<String, String>)
+//                authentication.getUserAuthentication().getDetails();
+//        session.setAttribute("user", User.builder()
+//                .name(map.get("name"))
+//                .email(map.get("email"))
+//                .principal(map.get("id"))
+//                .socialType(SocialType.FACEBOOK)
+//                .createdDate(LocalDateTime.now())
+//                .build()
+//        );
+//        return "redirect:/board/list";
+//    }
+
+    @GetMapping(value = "/{facebook|google|kakao|naver}/complete")
+    public String loginComplete(@SocialUser User user) {
+        return "redirect:/board/list";
+    }
+}
